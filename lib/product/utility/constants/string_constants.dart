100a028d8156edd9c1058b94da9399225e23f09f
Veli Bacik <vbacik.10@gmail.com>
2023-09-10 03:42:30 +0300

    added first verison testflight

--- lib/product/utility/constants/string_constants.dart
+++ lib/product/utility/constants/string_constants.dart
@@ -2,7 +2,6 @@ class StringConstants {
   static const String asteriks = '*';
   static const String phoneHint = '(53X) XX XX';
   static const String twitter = 'Twitter';
-  static const String afad = 'AFAD';
   static const String instagram = 'Instagram';
 
   /// Social Media Urls
