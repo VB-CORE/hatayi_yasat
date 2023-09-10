319c5eb73f58a0965dd05cdc77f54fee53230ac5
Veli Bacik <vbacik.10@gmail.com>
2023-09-10 03:43:55 +0300

    added first verison testflight

---
+++ lib/product/utility/constants/duration_constant.dart
@@ -0,0 +1,6 @@
+final class DurationConstant {
+  DurationConstant._();
+
+  static const Duration durationNormal = Duration(seconds: 1);
+  static const Duration durationMedium = Duration(seconds: 2);
+}
