a930ba25aaea2d8ab785c338bc5ae072ec5fbc71
Veli Bacik <vbacik.10@gmail.com>
2023-09-11 03:05:06 +0300

    added latest code

--- lib/product/widget/package/image_manipulation/image_manipulation.dart
+++ lib/product/widget/package/image_manipulation/image_manipulation.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img;
-import 'package:path_provider/path_provider.dart';
 import 'package:vbaseproject/product/generated/assets.gen.dart';
 
 @immutable
@@ -16,20 +15,12 @@ final class ImageManipulation {
   /// Adds a watermark to the image.
   Future<File?> addWatermark({required File file}) async {
     final image = img.decodeImage(file.readAsBytesSync());
-    // final image = await compute<Uint8List, img.Image?>(img.decodeImage, bytes);
     final watermark = await _getWatermarkImage(image);
     if (image == null || watermark == null) return null;
 
-    /// (target, watermark, center)
     img.compositeImage(image, watermark, center: true);
     final tempDir = Directory.systemTemp;
     final tempImagePath = tempDir.path.withEmptyWaterMark;
-
-    // await compute<MapEntry<String, img.Image>, bool>(
-    //   (message) => img.encodePngFile(message.key, message.value),
-    //   MapEntry(tempImagePath, image),
-    // );
-    // await img.encodePngFile(tempImagePath, image);
     File(tempImagePath).writeAsBytesSync(img.encodePng(image));
     return File(tempImagePath);
   }
@@ -37,24 +28,10 @@ final class ImageManipulation {
   /// We resize our watermark image according to the image size to be merged.
   Future<img.Image?> _getWatermarkImage(img.Image? image) async {
     if (image == null) return null;
-    // final wmFile = await _createFileFromAsset(Assets.icons.icWatermark.path);
     final vmFile2 = img
         .decodeImage(await _createFileFromBytes(Assets.icons.icWatermark.path));
-    // final watermark = await img.decodeImageFile(wmFile.path);
     if (vmFile2 == null) return null;
     final result = img.copyResize(vmFile2, width: image.width);
-    return result;
-  }
-
-  /// To read the file from assets and convert it into an image
-  Future<File> _createFileFromAsset(String assetPath) async {
-    final data = await rootBundle.load(assetPath);
-    final bytes = data.buffer.asUint8List();
-    final tempDir = await getTemporaryDirectory();
-    final tempPath = tempDir.path;
-    final tempFile = File('$tempPath/${assetPath.split('/').last}');
-    tempFile.writeAsBytesSync(bytes);
-    final result = File(tempFile.path);
     return result;
   }
 
