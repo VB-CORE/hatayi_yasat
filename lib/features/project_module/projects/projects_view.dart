d40d85afe1d5c21743deecd3a081359b8387c5b8
Berk Sartık <48359825+BerkSartik@users.noreply.github.com>
2023-09-17 17:16:48 +0300

    added notfoundlottie instead of placeholder

--- lib/features/project_module/projects/projects_view.dart
+++ lib/features/project_module/projects/projects_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vbaseproject/product/widget/lottie/not_found_lottie.dart';
 
 class ProjectsView extends StatefulWidget {
   const ProjectsView({super.key});
@@ -10,6 +11,6 @@ class ProjectsView extends StatefulWidget {
 class _ProjectsViewState extends State<ProjectsView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const NotFoundLottie();
   }
 }
--- lib/features/request/project/request_project_view.dart
+++ lib/features/request/project/request_project_view.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:vbaseproject/product/init/language/locale_keys.g.dart';
+import 'package:vbaseproject/product/widget/lottie/not_found_lottie.dart';
 
 class RequestProjectView extends StatefulWidget {
   const RequestProjectView({super.key});
@@ -16,6 +17,7 @@ class _RequestProjectViewState extends State<RequestProjectView> {
       appBar: AppBar(
         title: const Text(LocaleKeys.projectRequest_title).tr(),
       ),
+      body: const NotFoundLottie(),
     );
   }
 }
