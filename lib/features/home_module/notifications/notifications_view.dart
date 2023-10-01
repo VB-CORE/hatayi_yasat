2c87485fd703cd8ed6ca9309eb2faa62c6760839
Berk Sartık <48359825+BerkSartik@users.noreply.github.com>
2023-10-01 07:18:49 +0300

    notification nav updated

--- lib/features/home_module/notifications/notifications_view.dart
+++ lib/features/home_module/notifications/notifications_view.dart
@@ -1,13 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kartal/kartal.dart';
 import 'package:life_shared/life_shared.dart';
 import 'package:vbaseproject/features/home_module/notifications/notification_mixin.dart';
 import 'package:vbaseproject/product/init/language/locale_keys.g.dart';
+import 'package:vbaseproject/product/model/enum/notification_type.dart';
 import 'package:vbaseproject/product/utility/firebase/messaging_navigate.dart';
 import 'package:vbaseproject/product/utility/notifier/loading_notifier.dart';
 import 'package:vbaseproject/product/utility/package/shimmer/place_shimmer_list.dart';
 import 'package:vbaseproject/product/utility/padding/page_padding.dart';
+import 'package:vbaseproject/product/utility/size/icon_size.dart';
 import 'package:vbaseproject/product/widget/lottie/not_found_lottie.dart';
 
 class NotificationsView extends StatefulWidget {
@@ -41,14 +45,26 @@ class _NotificationsViewState extends State<NotificationsView>
             onTap: () async {
               if (loadingNotifier.value) return;
               showLoading();
-              await MessagingNavigate.instance.detailModelCheckAndNavigate(
-                context: context,
-                id: model.id,
-                customService: customService,
-              );
+              if (model.type == AppNotificationType.campaign) {
+                await MessagingNavigate.instance
+                    .detailModelCampaignCheckAndNavigate(
+                  context: context,
+                  id: model.id,
+                  customService: customService,
+                );
+              } else {
+                await MessagingNavigate.instance.detailModelCheckAndNavigate(
+                  context: context,
+                  id: model.id,
+                  customService: customService,
+                );
+              }
+
               hideLoading();
             },
+            leading: _NotificationTypeLeadingIcon(model: model),
             title: Text(model.title ?? ''),
+            dense: true,
             subtitle: Text(model.body ?? ''),
             trailing: const Icon(Icons.chevron_right_outlined),
           );
@@ -56,6 +72,21 @@ class _NotificationsViewState extends State<NotificationsView>
         // ...
       ),
     );
+  }
+}
+
+class _NotificationTypeLeadingIcon extends StatelessWidget {
+  const _NotificationTypeLeadingIcon({
+    required this.model,
+  });
+
+  final AppNotificationModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return model.type == AppNotificationType.campaign
+        ? const Icon(Icons.campaign_outlined)
+        : const Icon(Icons.home_outlined);
   }
 }
 
