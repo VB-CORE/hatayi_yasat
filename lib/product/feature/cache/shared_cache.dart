74ced3defd94d3aa5a5cae28181618460950cd29
Emre Gültekir <gltkr.emre@gmail.com>
2023-10-17 02:50:50 +0300

    favorite place cache operations added.

--- lib/product/feature/cache/shared_cache.dart
+++ lib/product/feature/cache/shared_cache.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+import 'package:vbaseproject/features/home_module/home_detail/models/favorite_place_model.dart';
 import 'package:vbaseproject/product/feature/cache/shared_keys.dart';
 import 'package:vbaseproject/product/feature/cache/shared_operation/base_shared_operation.dart';
 
@@ -42,5 +46,50 @@ final class SharedCache {
     final time = _sharedOperation.getValue<String>(SharedKeys.applyScholarship);
     if (time == null) return null;
     return DateTime.parse(time);
+  }
+
+  Future<bool> setFavoritePlace(FavoritePlaceModel favoritePlace) async {
+    final places = getFavoritePlaces()..add(favoritePlace);
+
+    //safeJsonEncodeCompute not working for encoding..
+    final jsonPlaces = places.map((place) => jsonEncode(place.toJson()));
+
+    return _sharedOperation.setStringList(
+      SharedKeys.favoritePlaces,
+      jsonPlaces.toList(),
+    );
+  }
+
+  Future<bool> removeFromFavorites(FavoritePlaceModel favoritePlace) async {
+    final places = getFavoritePlaces()
+      ..removeWhere((place) => place.name == favoritePlace.name);
+
+    //safeJsonEncodeCompute not working for encoding..
+    final jsonPlaces = places.map((place) => jsonEncode(place.toJson()));
+
+    return _sharedOperation.setStringList(
+      SharedKeys.favoritePlaces,
+      jsonPlaces.toList(),
+    );
+  }
+
+  bool isFavoritePlace(String name) {
+    final place =
+        getFavoritePlaces().firstWhereOrNull((element) => element.name == name);
+
+    return place != null;
+  }
+
+  List<FavoritePlaceModel> getFavoritePlaces() {
+    final places = _sharedOperation.getStringList(SharedKeys.favoritePlaces);
+
+    if (places == null) return [];
+
+    return places.map(
+      (place) {
+        final json = jsonDecode(place);
+        return FavoritePlaceModel.fromJson(json as Map<String, dynamic>);
+      },
+    ).toList();
   }
 }
