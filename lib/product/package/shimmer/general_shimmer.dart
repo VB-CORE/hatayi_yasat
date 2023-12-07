15e6742740f918a12fa0fbcca0e98ab9ed0d73bc
Berk Sartık <48359825+BerkSartik@users.noreply.github.com>
2023-12-07 07:51:41 +0300

    general shimmer updated with custom shimmer height

--- lib/product/package/shimmer/general_shimmer.dart
+++ lib/product/package/shimmer/general_shimmer.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:vbaseproject/product/utility/decorations/custom_shimmer_height.dart';
 import 'package:vbaseproject/product/utility/padding/page_padding.dart';
 import 'package:vbaseproject/product/widget/size/widget_size.dart';
 
-/// This is a shimmer that contains 10 items consisting of .24 height box.
+/// This shimmer contains 10 items and is created with custom height.
+/// Provides this height with context.sized.dynamicHeight() from Kartal.
 final class GeneralShimmer extends StatelessWidget {
-  const GeneralShimmer({super.key});
+  const GeneralShimmer({required this.height, super.key});
+
+  /// Use [CustomShimmerHeight] for this value.
+  final double height;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +29,7 @@ final class GeneralShimmer extends StatelessWidget {
               context.general.colorScheme.onPrimaryContainer.withOpacity(0.2),
           child: Container(
             alignment: Alignment.center,
-            height: context.sized.dynamicHeight(.24),
+            height: context.sized.dynamicHeight(height),
             width: context.sized.width,
             color: context.general.colorScheme.secondary,
           ),
