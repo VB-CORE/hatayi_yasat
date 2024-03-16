08a874c5c911ec32c3b64348187e0deb243436b3
vb10 <vbacik.10@gmail.com>
2024-03-16 17:58:38 +0300

    Update test case times and add extension for store model name

--- lib/product/utility/extension/store_model_etension.dart
+++ lib/product/utility/extension/store_model_etension.dart
@@ -1,5 +1,8 @@
+import 'package:kartal/kartal.dart';
 import 'package:life_shared/life_shared.dart';
 
 extension StoreModelExtension on StoreModel {
-  String get updatedName => name.toUpperCase();
+  String get updatedName {
+    return name.ext.toTitleCase();
+  }
 }
