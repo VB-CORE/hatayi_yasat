1623109211b1335afec593491bd1fde5f8a03244
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2024-03-17 20:32:53 +0300

    Add AndroidManifest.xml queries and update link_detail_view.dart

---
+++ lib/product/utility/uri/uri_utility.dart
@@ -0,0 +1,28 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class UriUtility {
+  UriUtility({
+    required this.address,
+  });
+
+  static const _httpScheme = 'http://';
+  static const _httpsScheme = 'https://';
+
+  final String address;
+
+  Uri get toUri {
+    if (_containsScheme) {
+      return Uri.parse(address);
+    } else {
+      return Uri.parse('$_httpsScheme$address');
+    }
+  }
+
+  bool get _containsScheme {
+    return address.contains(_httpScheme) || address.contains(_httpsScheme);
+  }
+
+  void launch() {
+    launchUrl(toUri);
+  }
+}
