d6c7d3bb77b51dca4f1970253a0bfd1c3e0357f4
Kadir Bekar <kadir.bekr@gmail.com>
2024-06-02 17:46:33 +0300

    [255_tourism_module]
    android, ios api keys added
    android didnt work as expected, it will be checked,
    dummy map view added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyBRKe2xH8AfmCquhe6jNhKpr46mq7EWgVw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/sub_feature/tourism/view/tourism_map_view.dart
+++ lib/sub_feature/tourism/view/tourism_map_view.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TourismMapView extends StatefulWidget {
   const TourismMapView({super.key});
@@ -8,10 +11,38 @@ class TourismMapView extends StatefulWidget {
 }
 
 class _TourismMapViewState extends State<TourismMapView> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.151926040649414,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Map View')),
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: _controller.complete,
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
