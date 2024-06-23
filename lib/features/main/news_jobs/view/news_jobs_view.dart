247c3cc7c38812e34caf29780ba9e7db5313c6b4
vb10 <vbacik.10@gmail.com>
2024-06-23 18:56:35 +0300

    Refactor widget imports for better organization and readability

--- lib/features/main/news_jobs/view/news_jobs_view.dart
+++ lib/features/main/news_jobs/view/news_jobs_view.dart
@@ -1,11 +1,28 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+import 'package:life_shared/life_shared.dart';
 import 'package:lifeclient/features/main/news_jobs/view/sub_view/tab_jobs_view.dart';
 import 'package:lifeclient/features/main/news_jobs/view/sub_view/tab_news_view.dart';
 import 'package:lifeclient/product/init/language/locale_keys.g.dart';
+import 'package:lifeclient/product/utility/decorations/index.dart';
 
 part 'widget/news_jobs_tab_bar.dart';
 part 'widget/news_jobs_tab_view.dart';
+
+enum NewsJobTabs {
+  news,
+  jobs;
+
+  String get title {
+    switch (this) {
+      case NewsJobTabs.news:
+        return LocaleKeys.navigationTabs_news.tr();
+      case NewsJobTabs.jobs:
+        return LocaleKeys.navigationTabs_advertise.tr();
+    }
+  }
+}
 
 final class NewsJobsView extends StatefulWidget {
   const NewsJobsView({super.key});
@@ -16,9 +33,9 @@ final class NewsJobsView extends StatefulWidget {
 class _NewsJobsViewState extends State<NewsJobsView> {
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
-      length: 2,
-      child: Scaffold(
+    return DefaultTabController(
+      length: NewsJobTabs.values.length,
+      child: const Scaffold(
         body: Column(
           children: [
             _NewsJobsTabBar(),
--- lib/features/main/news_jobs/view/widget/news_jobs_tab_bar.dart
+++ lib/features/main/news_jobs/view/widget/news_jobs_tab_bar.dart
@@ -1,16 +1,118 @@
 part of '../news_jobs_view.dart';
 
 @immutable
-final class _NewsJobsTabBar extends StatelessWidget {
+final class _NewsJobsTabBar extends StatefulWidget {
   const _NewsJobsTabBar();
 
   @override
+  State<_NewsJobsTabBar> createState() => _NewsJobsTabBarState();
+}
+
+class _NewsJobsTabBarState extends State<_NewsJobsTabBar>
+    with _NewsJobsTabMixin {
+  @override
   Widget build(BuildContext context) {
-    return TabBar(
-      tabs: [
-        Tab(text: LocaleKeys.navigationTabs_news.tr()),
-        Tab(text: LocaleKeys.navigationTabs_advertise.tr()),
-      ],
+    return Padding(
+      padding: const PagePadding.all(),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: context.general.colorScheme.onPrimaryFixed,
+          borderRadius: CustomRadius.medium,
+        ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Stack(
+              children: [
+                AnimatedPositioned(
+                  bottom: WidgetSizes.spacingXxs,
+                  top: WidgetSizes.spacingXxs,
+                  left: _leftPosition,
+                  right: _rightPosition,
+                  width: constraints.maxWidth / 2,
+                  duration: Durations.long1,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: context.general.colorScheme.secondary,
+                      borderRadius: CustomRadius.medium,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: _CustomTabButton(
+                        tab: NewsJobTabs.news,
+                        onPressed: () =>
+                            _changeCurrentTabView(NewsJobTabs.news),
+                        selectedTab: _currentTab,
+                      ),
+                    ),
+                    Expanded(
+                      child: _CustomTabButton(
+                        tab: NewsJobTabs.jobs,
+                        onPressed: () =>
+                            _changeCurrentTabView(NewsJobTabs.jobs),
+                        selectedTab: _currentTab,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+mixin _NewsJobsTabMixin on State<_NewsJobsTabBar> {
+  void _changeCurrentTabView(NewsJobTabs tab) {
+    DefaultTabController.maybeOf(context)?.animateTo(tab.index);
+    setState(() {
+      _currentTab = tab;
+    });
+  }
+
+  NewsJobTabs _currentTab = NewsJobTabs.news;
+
+  /// Returns the left position of the selected tab indicator.
+  double? get _leftPosition =>
+      _currentTab == NewsJobTabs.jobs ? null : WidgetSizes.spacingXxs;
+
+  /// Returns the right position of the selected tab indicator.
+  double? get _rightPosition =>
+      _currentTab == NewsJobTabs.news ? null : WidgetSizes.spacingXxs;
+}
+
+final class _CustomTabButton extends StatelessWidget {
+  const _CustomTabButton({
+    required this.tab,
+    required this.onPressed,
+    required this.selectedTab,
+  });
+  final NewsJobTabs tab;
+  final VoidCallback onPressed;
+  final NewsJobTabs selectedTab;
+
+  TextStyle? _currentSelectedTextStyle(BuildContext context) =>
+      selectedTab == tab
+          ? context.general.textTheme.titleSmall
+          : context.general.textTheme.titleSmall
+              ?.copyWith(color: context.general.colorScheme.onSecondaryFixed);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: SizedBox(
+        height: WidgetSizes.spacingXxl7,
+        child: Center(
+          child: Text(
+            tab.title,
+            style: _currentSelectedTextStyle(context),
+          ),
+        ),
+      ),
     );
   }
 }
