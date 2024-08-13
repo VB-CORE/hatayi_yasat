f6931c4b3dea53bbe7994a34547a6511e733bbf6
Berk Sartık <48359825+BerkSartik@users.noreply.github.com>
2024-08-13 20:25:40 +0300

    feature/#275

---
+++ lib/features/sub_feature/useful_links/provider/useful_links_view_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'useful_links_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$usefulLinksViewModelHash() =>
+    r'02a7869d452e770c783e3a8daaf71c2173a1965c';
+
+/// See also [UsefulLinksViewModel].
+@ProviderFor(UsefulLinksViewModel)
+final usefulLinksViewModelProvider = AutoDisposeNotifierProvider<
+    UsefulLinksViewModel, UsefulLinksState>.internal(
+  UsefulLinksViewModel.new,
+  name: r'usefulLinksViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$usefulLinksViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UsefulLinksViewModel = AutoDisposeNotifier<UsefulLinksState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
