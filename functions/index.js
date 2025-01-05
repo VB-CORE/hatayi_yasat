b2963b0adbff797ef4e7a0efc6c4802a236d28ba
vb10 <vbacik.10@gmail.com>
2025-01-05 08:31:51 +0300

    chore: Enhance Firebase emulator setup and update Firestore rules
    - Added emulator data export and import paths in firebase.json for better emulator management.
    - Updated pubspec.yaml with new script for starting Firebase emulators with data import/export.
    - Introduced new Firestore rules to allow public read access and restricted write access on the adBoard collection.
    - Refactored Firebase functions to use updated syntax for onDocumentCreated and onDocumentDeleted triggers.
    - Added a method to inject test environment during application initialization for improved debugging.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const { setGlobalOptions } = require("firebase-functions/v2");
 const axios = require("axios");
 const functions = require("firebase-functions");
-// const { onDocumentCreated } = require("firebase-functions/firestore");
+const { onCall } = require("firebase-functions/v2/https");
+const {
+  onDocumentCreated,
+  onDocumentDeleted,
+} = require("firebase-functions/v2/firestore");
 
 setGlobalOptions({ maxInstances: 10 });
 
@@ -22,7 +26,8 @@ const ADD_PATH = "/application_insert";
 const DELETE_PATH = "/delete_application";
 axios.defaults.headers.common["SECRET"] = SECRET;
 
-exports.search = functions.https.onCall(async (data, _) => {
+exports.search = onCall(async (request) => {
+  const { data } = request;
   const { term } = data;
   const target = 0;
   const page = -1;
@@ -49,13 +54,13 @@ exports.search = functions.https.onCall(async (data, _) => {
   }
 });
 
-exports.addToIndexMongo = functions.firestore
-  .document("/approvedApplications/{documentId}")
-  .onCreate(async (change, context) => {
-    const original = change.data();
+exports.addToIndexMongo = onDocumentCreated(
+  "/approvedApplications/{documentId}",
+  async (event) => {
+    const original = event.data.data();
     const target = 0;
     const jsonBody = JSON.stringify({
-      id: change.id,
+      id: event.data.id,
       ...original,
     });
     const mongoResponse = await axios.post(BASE_URL + ADD_PATH, jsonBody, {
@@ -63,15 +68,17 @@ exports.addToIndexMongo = functions.firestore
     });
 
     return mongoResponse != null;
-  });
+  }
+);
 
-exports.removeToIndexMongo = functions.firestore
-  .document("/approvedApplications/{documentId}")
-  .onDelete(async (change, context) => {
+exports.removeToIndexMongo = onDocumentDeleted(
+  "/approvedApplications/{documentId}",
+  async (event) => {
     const target = 0;
     const mongoResponse = await axios.delete(BASE_URL + DELETE_PATH, {
-      params: { target, id: change.id },
+      params: { target, id: event.data.id },
     });
 
     return mongoResponse != null;
-  });
+  }
+);
