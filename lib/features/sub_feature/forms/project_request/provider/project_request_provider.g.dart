d77e3f9f7065b3b0168b6d3dc2c3359d2da038ad
Veli Bacik <vbacik.10@gmail.com>
2025-09-15 23:06:34 +0300

    chore: Update version numbers and test execution times
    - Updated version number in pubspec.yaml from v5.4.10 to v5.4.2 for dependency management.
    - Incremented VERSION_NUMBER in Fastfile for both Android and iOS from 8.1.0 to 8.1.1.
    - Adjusted CURRENT_PROJECT_VERSION in Info.plist and project.pbxproj from 65 to 66 for iOS.
    - Updated test execution times in report.xml files for both Android and iOS to reflect recent performance metrics.

--- lib/features/sub_feature/forms/project_request/provider/project_request_provider.g.dart
+++ lib/features/sub_feature/forms/project_request/provider/project_request_provider.g.dart
@@ -6,6 +6,9 @@ part of 'project_request_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
 @ProviderFor(ProjectRequestProvider)
 const projectRequestProviderProvider = ProjectRequestProviderProvider._();
 
@@ -56,6 +59,3 @@ abstract class _$ProjectRequestProvider extends $Notifier<ProjectRequestState> {
     element.handleValue(ref, created);
   }
 }
-
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/features/sub_feature/useful_links/provider/useful_links_view_model.g.dart
+++ lib/features/sub_feature/useful_links/provider/useful_links_view_model.g.dart
@@ -6,6 +6,9 @@ part of 'useful_links_view_model.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
 @ProviderFor(UsefulLinksViewModel)
 const usefulLinksViewModelProvider = UsefulLinksViewModelProvider._();
 
@@ -56,6 +59,3 @@ abstract class _$UsefulLinksViewModel extends $Notifier<UsefulLinksState> {
     element.handleValue(ref, created);
   }
 }
-
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
