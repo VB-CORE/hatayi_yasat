a22e450dfe67e39b2c97801167fd7fa943b5b7cf
Can Kurt <bencankurt17@gmail.com>
2026-07-20 21:52:46 +0300

    refactor(like_button): simplify color handling in LikeButton widget

--- lib/product/widget/button/like_button.dart
+++ lib/product/widget/button/like_button.dart
@@ -16,20 +16,20 @@ final class CustomAnimatedLikeButton extends StatelessWidget {
   final Widget Function(bool isLiked)? likeBuilder;
   final Future<bool?> Function(bool isLiked)? onTap;
 
+  Color get fromColor => AppColors.coral400;
+  Color get toColor => AppColors.coral600;
+
   @override
   Widget build(BuildContext context) {
     return LikeButton(
       size: size,
-      circleColor: const CircleColor(
-        start: AppColors.coral400,
-        end: AppColors.coral600,
-      ),
+      circleColor: CircleColor(start: fromColor, end: toColor),
       isLiked: isLiked,
       likeCountPadding: EdgeInsets.zero,
       animationDuration: const Duration(milliseconds: 800),
-      bubblesColor: const BubblesColor(
-        dotPrimaryColor: AppColors.coral400,
-        dotSecondaryColor: AppColors.coral600,
+      bubblesColor: BubblesColor(
+        dotPrimaryColor: fromColor,
+        dotSecondaryColor: toColor,
       ),
       likeBuilder: likeBuilder,
       onTap: onTap,
