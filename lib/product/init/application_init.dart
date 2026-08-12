2e9c242cedccfd4f695e993ba0d0d12e031d7d0c
Muhammed Emin Alan <82614780+eminalan@users.noreply.github.com>
2026-08-12 22:10:28 +0300

    chore(init): drop firebase imports left unused by the emulator setup

--- lib/product/init/application_init.dart
+++ lib/product/init/application_init.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 import 'dart:isolate';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
