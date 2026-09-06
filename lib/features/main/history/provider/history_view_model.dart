4d60bc2050b7fed9cbd319ddf0044e5f8b8b879b
Veli Bacik <vbacik.10@gmail.com>
2026-09-06 19:06:48 +0300

    refactor(history): remove unused MemoryEmptyModel and clean up fetchMemoriesQuery

--- lib/features/main/history/provider/history_view_model.dart
+++ lib/features/main/history/provider/history_view_model.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:life_shared/life_shared.dart';
 import 'package:lifeclient/core/dependency/project_dependency_mixin.dart';
 import 'package:lifeclient/features/main/history/provider/history_state.dart';
-import 'package:lifeclient/product/model/memory_empty_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'history_view_model.g.dart';
@@ -17,14 +16,9 @@ final class HistoryViewModel extends _$HistoryViewModel
 
   Query<MemoryModel?> fetchMemoriesQuery() {
     return firestoreService.collectionReference(
-      CollectionPaths.memories, // Temporary - should be memories
+      CollectionPaths.memories,
       MemoryModel.empty(),
     );
-  }
-
-  // TEMP: Mock data için geçici method - test amaçlı
-  List<MemoryModel> getMockMemories() {
-    return MemoryEmptyModel.mockMemories;
   }
 
   void updateSelectedMemoryIndex(int index) {
